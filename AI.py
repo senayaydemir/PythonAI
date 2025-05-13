155a212c3d83c0765cdcabe1f073ff6544eb99c6
senayaydemir <senayaydemir00@gmail.com>
2025-05-13 23:59:22 +0300

    Güncelleme Yapıldı

--- AI.py
+++ AI.py
@@ -2,3 +2,4 @@ import json
 from difflib import get_close_matches as yakin_sonuclari_getir
 def veritabani_yukle():
     with open('C:\\Users\\SENAY\\Desktop\\PythonAI\\veritabani.json','r') as dosya:
+        return json.load(dosya)
