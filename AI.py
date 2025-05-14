c181550c4c6f86b349a620650ceb6c01a53104e3
senayaydemir <senayaydemir00@gmail.com>
2025-05-14 23:51:55 +0300

    veritabanına yaz eklendi

--- AI.py
+++ AI.py
@@ -3,3 +3,7 @@ from difflib import get_close_matches as yakin_sonuclari_getir
 def veritabani_yukle():
     with open('C:\\Users\\SENAY\\Desktop\\PythonAI\\veritabani.json','r') as dosya:
         return json.load(dosya)
+def veritabaina_yaz(veriler):    
+    
+    with open('C:\\Users\\SENAY\\Desktop\\PythonAI\\veritabani.json','w') as dosya:
+        json.dump(veriler,dosya,indent=2)
